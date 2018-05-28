26a4642f14aab2582fe3e41b5a627b733d2e415f
Yury Markouski <everest2006@tut.by>
2018-05-28 18:01:10 +0300

    create function 'questionFunc'

--- secondLesson/index.js
+++ secondLesson/index.js
@@ -6,3 +6,18 @@ let pow = (x,n) => {
     return Math.pow(x,n);
 };
 
+let questionFunc = (a) => {
+    let a = +prompt('a?', '');
+    switch (a) {
+        case 0: alert( 0 );
+                break;
+        case 1: alert( 1 );
+                break;
+        case 2:
+
+        case 3: alert( '2,3' );
+                     break;
+        default: alert( 'not found' );
+    }
+};
+
