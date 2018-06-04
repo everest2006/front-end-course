0c31929dc71763d1c6673de44e7253726c1b15da
Yury Markouski <everest2006@tut.by>
2018-06-04 23:12:14 +0300

    add function ransformString

--- fourthLesson/index.js
+++ fourthLesson/index.js
@@ -6,4 +6,7 @@ let modifyArr = (arr, el, newEl) => {
   return arr.some(value=>value===el) ? arr.map( value => value === el?newEl:value) : `element ${el} not found in array [${arr}]`
 };
 
+let transformString = (str, el, newEl) => {
+  return str.toLowerCase().indexOf(el.toLowerCase())!==-1 ? str.replace(new RegExp(el, 'gi'), newEl) : `element ${el} not found in string ${str}`;
+};
 
