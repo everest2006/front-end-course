42176a17a7cc0846950de52d9af39523fdcdc72a
Yury Markouski <everest2006@tut.by>
2018-06-04 22:40:56 +0300

    add function modifyArr

--- fourthLesson/index.js
+++ fourthLesson/index.js
@@ -2,3 +2,8 @@ let isInArray = (arr, el) => {
     return arr.some(value=>value===el);
 };
 
+let modifyArr = (arr, el, newEl) => {
+  return arr.some(value=>value===el) ? arr.map( value => value === el?newEl:value) : `element ${el} not found in array [${arr}]`
+};
+
+
